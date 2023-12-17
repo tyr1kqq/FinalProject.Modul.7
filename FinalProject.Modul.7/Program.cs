945aaa353f198428c40fceb29e809f830da6ee73
tyr1kqq <igorturkinwhite@yandex.ru>
2023-12-17 13:26:57 +0200

    промежуточный проект

--- FinalProject.Modul.7/Program.cs
+++ FinalProject.Modul.7/Program.cs
@@ -1,88 +1,173 @@
-﻿using System.Linq.Expressions;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Channels;
-using System.Xml.Linq;
+﻿using System.Globalization;
 
 namespace FinalProject.Modul._7
 {
-    
-    abstract class Delivery
-    {
-        public string Address;
-    }
 
-    abstract class DeliveryRegoin : Delivery
+    abstract class DeliveryInfo
     {
-        public string[] Region = { "Moscow region", "Yaroslavl region", "Kalininrad region" };
+        public string Adress;
+        public string CityAdress;
+        public string[] CartAdd;
+       
+        public static string[] City = { "Moscow", "Kaluga", "Kaliningrad" };
+        public static string[] Cart = { "Mouse", "KeyBoard", "MotherBoard", "Computer" };
+       
+    }
+    abstract class Delivery : DeliveryInfo
+    {
+        public abstract void DeliveryToAdress(in int adress, out bool IsDelivery);
+        public bool IsDelivery;
+    }
+    abstract class DeliveryCartInfo : DeliveryInfo
+    {
+         public string[] CartInfo;
+        bool IsAddCart= false;
         
+
+        public void InfoFromConsole()
+        {
+            if (CartInfo == null)
+            {
+                Console.WriteLine("Ваша корзина пуста , хотите что-то добвить?\nYes or No...");
+                var YesOrNo = Console.ReadLine();
+                if (YesOrNo == "Yes")
+                {
+
+                    Console.WriteLine("Что добавляем из предлоденого?");
+                    foreach (var item in Cart)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    var NewCart = Console.ReadLine();
+
+                    do
+                    {
+                        NewCart = Console.ReadLine();
+                        if (CartInfo.Contains(NewCart))
+                        {
+                            IsAddCart = false;
+                        }
+                        else 
+                        {
+                            Console.WriteLine("Введите название товара коректно");
+                            IsAddCart = true; 
+                        }
+                    }
+                    while (IsAddCart);
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("В вашей крзине следующие товары:");
+                foreach (var item in CartInfo)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
     }
-
-    abstract class DeliveryCity : DeliveryRegoin
-    {
-        public string[] CityInMoscowRegion = { "Moscow" , "Himki" , "Podolsk" };
-        public string[] CityInYaroslavlRegion = { "Yaroslavl", "Ribinsk", "Danilov" };
-        public string[] CityInKaliningradRegion = { "Kaliningrad", "Gvardeysk", "Baltiysk" };
-    }
-
-    
-
+   
     class HomeDelivery : Delivery
     {
-       
+        public override void DeliveryToAdress(in int adress, out bool IsDelivery)
+        {
+            if (City.Contains(CityAdress))
+            {
+                Console.WriteLine("Доставка в ваш город возможна\nНачинаем оформление и доставку заказа");
+                IsDelivery = true;
+                
+            }
+            else
+            {
+                Console.WriteLine("Мы не сможем доставить в ваш город");
+                IsDelivery = false;
+            }
+        }
     }
 
     class PickPointDelivery : Delivery
     {
-        /* ... */
+        
+        
+        public override void DeliveryToAdress(in int adress, out bool IsDelivery)
+        {
+          
+            if (City.Contains(CityAdress))
+            {
+                Console.WriteLine("Доставка в ваш город возможна\nНачинаем оформление и доставку заказа");
+                IsDelivery = true;        
+            }
+            else
+            {
+                Console.WriteLine("Мы не сможем доставить в ваш город");
+                IsDelivery = false;
+            }
+        }
     }
 
     class ShopDelivery : Delivery
     {
-        /* ... */
-    }
-
-    class DeliveryInfo : DeliveryCity
-    {
-        public DeliveryInfo()
+        private string[] DeliveryShop = { "Moscow", "Sankt-Peterburg", "Yaroslavl" };
+        public override void DeliveryToAdress(in int adress, out bool IsDelivery)
         {
-            Console.WriteLine("Добро пожаловать в магазин \n сначала посмотри  город , куда возможна доставка");
-            Console.Write("Города в московской области: ");
-            foreach (var CityInMoscow in CityInMoscowRegion)
+            if (DeliveryShop.Contains(CityAdress))
             {
-                Console.Write(CityInMoscow + "  ");
+                Console.WriteLine("Доставка в ваш город возможна\nНачинаем оформление и доставку заказа");
+                IsDelivery = true;
             }
-            Console.WriteLine();
-            Console.Write("Города Ярославской области: ");
-            foreach (var CityInYaroslavl in CityInYaroslavlRegion)
+            else
             {
-                Console.Write(CityInYaroslavl + "  ");
+                Console.WriteLine("Мы не сможем доставить в ваш город");
+                IsDelivery = false;
             }
-            Console.WriteLine();
-            Console.Write("Города Калиниградской области:");
-            foreach (var CityInKaliningrad in CityInKaliningradRegion)
-            {
-                Console.Write(CityInKaliningrad + "  ");
-            }
-
-
-
-
         }
     }
-    class Order<TDelivery> where TDelivery : Delivery
+    
+
+    class Order<TDelivery,
+    TStruct> where TDelivery : Delivery
     {
         public TDelivery Delivery;
 
-       
-        // ... Другие поля
+        public int Number;
+
+        public string Description;
+
+        public string[] CityAdress;
+
+        public void DipslpayInfo()
+        {
+            Console.WriteLine(Delivery.Adress);
+        }
+         
     }
 
     internal class Programm
     {
         static void Main(string[] args)
         {
-            DeliveryInfo CityInfo = new DeliveryInfo();
-            Console.WriteLine(CityInfo);
+          HomeDelivery homeDelivery = new HomeDelivery();
+            homeDelivery.CityAdress = "Moscow";
+            homeDelivery.Adress = "Pushkina 10";
+
+            PickPointDelivery pickPointDelivery = new PickPointDelivery();
+            pickPointDelivery.CityAdress = "Kazan";
+            homeDelivery.Adress = "Gagarina 2";
+
+            ShopDelivery shopDelivery = new ShopDelivery();
+            shopDelivery.CityAdress = "Sankt-Peterburg";
+            shopDelivery.Adress = "Moskovskiy prospekt 49";
+
+            Order<HomeDelivery , string> order1 = new Order<HomeDelivery , string>();
+            order1.Delivery = homeDelivery;
+            order1.Number = 1;
+            order1.Description = "ABC";
+
+            order1.Display
+
         }
     }
 
